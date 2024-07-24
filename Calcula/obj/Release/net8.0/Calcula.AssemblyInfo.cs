8a602bbe0630841b7372a49eeaedb8b56be62b17
tienpham2k4 <146936917+tienpham2k4@users.noreply.github.com>
2024-07-24 18:03:20 +0700

    commited

--- Calcula/obj/Release/net8.0/Calcula.AssemblyInfo.cs
+++ Calcula/obj/Release/net8.0/Calcula.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calcula")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a67bb6651443def91f86dfd322efc0dee7cdca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calcula")]
